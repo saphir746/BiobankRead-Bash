0f703fe4168c914484e8baa719e557b80bc9362a
Bill Crum <w.crum@imperial.ac.uk>
2018-05-23 14:46:06 +0100

    Changed logic for parsing remove_outliers, aver_visits and remove_missing

--- extract_variables.py
+++ extract_variables.py
@@ -18,7 +18,7 @@ import re
         --vars <list of variables>, as is or in .txt file \
         --out <directory name> x1\y1 \
 (optionally)
-        --baseline_only True\False \
+        --baseline_only True\False (default=True)\
         --remove_missing True\False \
         --filter <list of conditions on variables in vars>, as is or in .txt file \
         --aver_visits True\False \
@@ -35,6 +35,19 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
+def str2boolorlist(v):
+    if type(v) == str:
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+    elif type(v) == list:
+        return v
+    else:
+        raise argparse.ArgumentTypeError('Boolean value or list expected.')
+
 parser = argparse.ArgumentParser(description="\n BiobankRead Extract_Variable. Does what it says hehe")
 
 in_opts = parser.add_argument_group(title='Input Files', description="Input files. The --csv and --html option are required")
@@ -50,12 +63,12 @@ out_opts.add_argument("--out", metavar='PREFIX', type=str, help='Specify the nam
 options = parser.add_argument_group(title="Optional input", description="Apply some level of selection on the data")
 options.add_argument("--baseline_only", type=str2bool, nargs='?', const=True, default=True,  help="Only keep data from baseline assessment centre")
 options.add_argument("--remove_missing", default=False, nargs='+', type=str,help="Remove subjects with values nan, -3 or -7 for any variable. Can specify which variables to perform that for")
-options.add_argument("--remove_outliers", default=False, nargs='+', type=str,action='store',help="Remove subjects with values beyond x std dev for any cont. variable. Format:[std.dev,one-sided,vars names...]")
+options.add_argument("--remove_outliers", type=str2bool, default=False, nargs='+',action='store',help="Remove subjects with values beyond x std dev for any cont. variable. Format:[std.dev,one-sided,vars names...]")
 options.add_argument("--filter", default=False, nargs='+', type=str,action='store',help="Filter some variables based on conditions. Keep your requests simple ")
 
 sums = parser.add_argument_group(title="Optional request for basic summary", description="Perform mean /cov/ corr/ dist plots for the data")
-sums.add_argument("--aver_visits",default=False,type=bool,help="get average measurement per visit")
-sums.add_argument("--cov_corr",default=False,type=bool,help="Produce extra file of cov/corr between variables. Will have same location and similar name to main output file")
+sums.add_argument("--aver_visits",default=False,type=str2boolorlist,help="get average measurement per visit")
+sums.add_argument("--cov_corr",default=False,type=str2bool,help="Produce extra file of cov/corr between variables. Will have same location and similar name to main output file")
 
 ########################################################
 
@@ -74,7 +87,7 @@ def actual_vars(smth):
         args.aver_visit if  > 1
         args.cov_corr if  > 1
         '''
-    actual_vars = []
+    actual_vars_list = []
     All=UKBr.Vars
     # turn strings to list
     if type(smth) is str:
@@ -88,11 +101,11 @@ def actual_vars(smth):
         else:
             res = [x for x in All if V in x]
         for i in res:
-            actual_vars.append(i)
-        if len(actual_vars)==0: 
+            actual_vars_list.append(i)
+        if len(actual_vars_list)==0: 
             ValueError('Variables names wrong. Go back to app documents and double-check what you actually have')
             return None
-    return actual_vars
+    return actual_vars_list
    
 def bad_chars(df):
     ''' Remove bad chars from Df column names '''
@@ -118,10 +131,11 @@ def remove(Df,args):
     return Df
 
 def average_visits(Df,args):
-    if args.aver_visits:
-        var_names = actual_vars(args.vars)
-    else:
+    if type(args.aver_visits) == list:
         var_names = actual_vars(args.aver_visits)
+    else:
+        var_names = actual_vars(args.vars)
+        
     types=[]
     for v in var_names:
         t=UKBr.variable_type(v)
@@ -144,14 +158,14 @@ def average_visits(Df,args):
 
 def outliers(Df,args):
     ''' Remove outliers from cont variables'''
-    if args.remove_outliers:
-        var_names = actual_vars(args.vars)
-        std=4
-        onesided=False
-    else:
+    if type(args.remove_outliers) == list:
         var_names = actual_vars(args.remove_outliers[2::])
         std = args.remove_outliers[0]
         onesided = args.remove_outliers[1]
+    else:
+        var_names = actual_vars(args.vars)
+        std=4
+        onesided=False
     types=[]
     for v in var_names:
         t=UKBr.variable_type(v)
@@ -267,14 +281,14 @@ def produce_plots(df,args):
 #args.csv='D:\UkBiobank\Application 10035\\21204\ukb21204.csv'
 #args.cov_corr=False
 #####
-
+import sys
 if __name__ == '__main__':
     args = parser.parse_args()
     
     namehtml=args.html
     namecsv=args.csv
     ### import Biobankread package
-   # sys.path.append('D:\new place\Postdoc\python\BiobankRead-Bash')
+    # sys.path.append('D:\new place\Postdoc\python\BiobankRead-Bash')
     try:
         import biobankRead2.BiobankRead2 as UKBr
         UKBr = UKBr.BiobankRead(html_file = namehtml, csv_file = namecsv)
@@ -286,6 +300,7 @@ if __name__ == '__main__':
             print("BBr loaded successfully")
         except:
             raise ImportError('UKBr could not be loaded properly')
+
     Df=extract_the_things(args)
     Df=float_to_cat(Df)
     final_name = args.out+'.csv'
