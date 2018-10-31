9e08aedad4cf20623a141b527a60cca0d7e82a90
saphir746 <ds711@imperial.ac.uk>
2018-10-31 14:00:51 +0000

    Update test-BBr-script.sh

--- test-BBr-script.sh
+++ test-BBr-script.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-#PBS -l walltime=06:00:00
-#PBS -l select=1:ncpus=4:mem=64gb
-#PBS -q med-bio
-#PBS -k oe
-#PBS -N BBR
 
+$WORK=<somewhere>
 
-$WORK=/work/ds711
-# Where the data is store'
-CSV_path=$WORK/UkBiobank/Application_236/R4528/ukb4528.csv
-HTML_path=$WORK/UkBiobank/Application_236/R4528/ukb4528.html
-TSV_path=$WORK/UkBiobank/HES_data/ukb_HES_236.tsv
+# UKb input data
+CSV_path=$WORK/ukb4528.csv
+HTML_path=$WORK/ukb4528.html
+TSV_path=$WORK/ukb_HES_236.tsv
+
 # Variables and conditions
-varList=$WORK/BBR/vars_test.txt ##("'Age assessment'" "'BMI'") ###$WORK/BBR/vars_test.txt 
-filterList=$WORK/BBR/filters_test.txt  #("Age assessment>50" "Age assessment<70" "BMI>=23" "BMI<=30")
-HES_codes=$WORK/BBR/ICD10_codes.txt
+varList=$WORK/vars_test.txt ##("'Age assessment'" "'BMI'") ###$WORK/BBR/vars_test.txt 
+filterList=$WORK/filters_test.txt  #("Age assessment>50" "Age assessment<70" "BMI>=23" "BMI<=30")
+HES_codes=$WORK/ICD10_codes.txt
 Death_codes=$HES_codes
-SR_things=$WORK/BBR/SR_disease.txt
+SR_things=$WORK/SR_disease.txt
+Withdrawn=$WORK/withdrawn_eids.csv
+
 # Output
 outname=test_hpc
 outname_HES=test_hpc_HES
@@ -25,14 +23,14 @@ outname_death=test_hpc_dead
 outname_SR=test_hpc_SR
 # Construct script path and arguments
 
-cp $WORK/BBR/* $TMPDIR
-module load anaconda3/personal
 source activate py36
+# environment previously created with Conda to install Python 3.6 and all the dependencies of BiobankRead
 
 python extract_variables.py \
 	--csv $CSV_path \
 	--html $HTML_path \
 	--vars $varList \
+	--excl $Withdrawn \
         #--filter $filterList \
         #--remove_outliers 'True' \
         #--baseline_only 'False' \
@@ -45,6 +43,7 @@ python HES_extract.py \
 	--codes $HES_codes \
 	--codeType 'ICD10' \
 	--dateTpe 'epistart' \
+	--excl $Withdrawn \
 	##--firstvisit 'True' \
         ##--baseline 'True' \
 	--out $outname_HES &
@@ -53,6 +52,7 @@ python extract_death.py \
         --csv $CSV_path \
         --html $HTML_path \
 	--codes $Death_codes \
+	--excl $Withdrawn \
 	##--primary 'True' \
         ##--secondary 'False' \
 	--out $outname_death &
@@ -63,9 +63,10 @@ python extract_SR.py \
         --html $HTML_path \
 	--disease $SR_things \
 	--SRcancer 'True' \
+	--excl $Withdrawn \
 	--out $outname_SR &
 
-cp *.csv $PBS_O_WORKDIR 
+# all results in .csv format
 
 
 
